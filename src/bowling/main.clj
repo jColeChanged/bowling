aa13d452a1ae1469e8f6c6fdb02b915598571d41
Joshua <jColeChanged@gmail.com>
2012-07-19 13:12:21 -0700

    Create the tables when they don't exist already.

--- src/bowling/main.clj
+++ src/bowling/main.clj
@@ -1,6 +1,8 @@
 (ns bowling.main
   (:use [bowling.views :only [start-app]])
+  (:use [bowling.models :only [create-game-table]])
   (:gen-class))
 
 (defn -main [& rest]
+  (create-game-table)
   (start-app :on-close :exit))
